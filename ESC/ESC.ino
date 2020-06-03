2d8a18fa0d9faa558ae2a664cbc8626fd33fb535
涂 家 浩 <35959817+Jahhow@users.noreply.github.com>
2020-06-03 15:54:48 +0800

    Fluent Motor Start

--- ESC/ESC.ino
+++ ESC/ESC.ino
@@ -4,8 +4,8 @@
 #define SPEED_DOWN        A1 // BLDC motor speed-down button
 #define SoundSensorPin    A5 // this pin read the analog voltage from the sound level meter
 #define PWM_MAX_DUTY     255
-#define PWM_MIN_DUTY      50
-#define PWM_START_DUTY    100
+#define PWM_MIN_DUTY       0
+#define PWM_START_DUTY    30
 #define ARRLEN            64
 #define VREF             5.0 // voltage on AREF pin,default:operating voltage
 
@@ -26,39 +26,23 @@ int Loss() {
   return getOriginalSoundLevel;
 }
 
-byte bldc_step = 0, pwm_duty=PWM_START_DUTY;
+byte bldc_step = 0, pwm_duty = PWM_START_DUTY;
+byte pwm[ARRLEN];
 unsigned int commutationCounter = 0;
-uint8_t pwm[ARRLEN];
-int innerEpoch = 5;
-int lose;
+unsigned int bldc_moveCount = 0;
+int innerEpoch = 5, lose;
 size_t iarr = 0;
+unsigned long ms = 0;
 
-void setup() {
-  DDRB = 0b1110;   // Configure pins 9(OC1A), 10(OC1B) and 11(OC2A) as outputs.  Used as PWM pins.
-  DDRD = 0b111000; // Configure pins 3, 4 and 5 as outputs.  Use as normal pins.
-
-  // Timer1 (no prescaling)
-  TCCR1B = 1;
-
-  // Timer2 (no prescaling)
-  TCCR2B = 1;
-
-  // Analog comparator setting
-  ACSR   = 0x10;           // Disable and clear (flag bit) analog comparator interrupt
-  pinMode(SPEED_UP,   INPUT_PULLUP);
-  pinMode(SPEED_DOWN, INPUT_PULLUP);
-
-  //Serial.begin(115200);
-}
 // Analog comparator ISR
 ISR (ANALOG_COMP_vect) {
   // BEMF debounce
-  for (int i = 0; i < 10; i++) {
-    while ((bldc_step & 1) != ((ACSR >> ACO) & 1)) // ACO: Analog Comparator Output
-      ;
+  for (int i = 0; i < 16; i++) {
+    if ((bldc_step & 1) != ((ACSR >> ACO) & 1))
+      return;
   }
+
   bldc_move();
-  bldc_step = (bldc_step + 1) % 6;
   /*if (++commutationCounter > 1000) {
     printOriginalSoundLevel();
     commutationCounter = 0;
@@ -66,120 +50,117 @@ ISR (ANALOG_COMP_vect) {
 }
 void bldc_move() {       // BLDC motor commutation function
   switch (bldc_step) {
-    case 0:
-      AH_BL();
-      BEMF_C_RISING();
-      break;
-    case 1:
-      AH_CL();
-      BEMF_B_FALLING();
-      break;
-    case 2:
-      BH_CL();
-      BEMF_A_RISING();
-      break;
-    case 3:
-      BH_AL();
-      BEMF_C_FALLING();
-      break;
-    case 4:
-      CH_AL();
-      BEMF_B_RISING();
-      break;
-    case 5:
-      CH_BL();
-      BEMF_A_FALLING();
-      break;
+    case 0: AH_BL(); BEMF_C_RISE(); break;
+    case 1: AH_CL(); BEMF_B_FALL(); break;
+    case 2: BH_CL(); BEMF_A_RISE(); break;
+    case 3: BH_AL(); BEMF_C_FALL(); break;
+    case 4: CH_AL(); BEMF_B_RISE(); break;
+    case 5: CH_BL(); BEMF_A_FALL(); break;
   }
+  ++bldc_moveCount;
+  bldc_step = (bldc_step + 1) % 6;
 }
 
-void loop() {
-  SET_PWM_DUTY(PWM_START_DUTY);    // Setup starting PWM with duty cycle = PWM_START_DUTY
-  // Motor start
-  
-  unsigned int i = 5000;
-  while (i > 20) {
-    bldc_move();
-    bldc_step = (bldc_step + 1) % 6;
-    delayMicroseconds(i);
-    i -= 30;
-  }
-  
+void setup() {
+  DDRB = 0b1110;   // Configure pins 9(OC1A), 10(OC1B) and 11(OC2A) as outputs.  Used as PWM pins.
+  DDRD = 0b111000; // Configure pins 3, 4 and 5 as outputs.  Use as normal pins.
+
+  TCCR1B = 1;// Timer1 (no prescaling)
+  TCCR2B = 1;// Timer2 (no prescaling)
+
+  pinMode(SPEED_UP,   INPUT_PULLUP);
+  pinMode(SPEED_DOWN, INPUT_PULLUP);
+
+  // Analog comparator setting
+  //ACSR  = 0x10; // Disable and clear (flag bit) analog comparator interrupt
   ACSR |= 0x08; // Enable analog comparator interrupt
-  while (1) {
-    while (!(digitalRead(SPEED_UP)) && pwm_duty < PWM_MAX_DUTY) {
-      pwm_duty++;
-      SET_PWM_DUTY(pwm_duty);
-      delay(100);
-    }
-    while (!(digitalRead(SPEED_DOWN)) && pwm_duty > PWM_MIN_DUTY) {
-      pwm_duty--;
-      SET_PWM_DUTY(pwm_duty);
-      delay(100);
-    }
 
-    // Noise Canceling Algorithm
-    // for (iarr = 0; iarr < ARRLEN; iarr++)
-    /* {
-      int direction = 1;
-      int loseDiff;
-      int v = 1;
-      lose = Loss();
-      //printf("\n iarr: %u, Loss %d ", iarr, lose);
-      for (size_t iInnerEpoch = 0; iInnerEpoch < innerEpoch; iInnerEpoch++)
-      {
-        uint8_t oldCurPwm = pwm[iarr];
-        pwm[iarr] = max(min((int)oldCurPwm + v, 255), 0);
-        int newLose = Loss();
-        loseDiff = newLose - lose;
-        //printf("%d ", newLose);
-        if (loseDiff >= 0)
-        {
-          pwm[iarr] = oldCurPwm;
-          direction = -direction;
-        }
-        lose = newLose;
-        if (loseDiff == 0)
-          break;
-        v = direction * abs(loseDiff);
-      }
-      }
-      iarr = (iarr + 1) & ~ARRLEN; // (iarr + 1) % ARRLEN
-    */
+  SET_PWM_DUTY(PWM_START_DUTY);    // Setup starting PWM with duty cycle = PWM_START_DUTY
+  Serial.begin(9600);
+}
+void loop() {
+  // Motor start
+  unsigned long curMs = millis();
+  unsigned long distanceMs = curMs - ms;
+  //Serial.println(distanceMs);
+  if (distanceMs > 10) {
+    if(bldc_moveCount==0){
+      bldc_move();
+    }
+    ms = curMs;
+    bldc_moveCount=0;
   }
+
+  if (pwm_duty < PWM_MAX_DUTY && !(digitalRead(SPEED_UP))) {
+    SET_PWM_DUTY(++pwm_duty);
+    delay(30);
+  }
+  if (pwm_duty > PWM_MIN_DUTY && !(digitalRead(SPEED_DOWN))) {
+    SET_PWM_DUTY(--pwm_duty);
+    delay(30);
+  }
+
+  // Noise Canceling Algorithm
+  // for (iarr = 0; iarr < ARRLEN; iarr++)
+  /* {
+    int direction = 1;
+    int loseDiff;
+    int v = 1;
+    lose = Loss();
+    //printf("\n iarr: %u, Loss %d ", iarr, lose);
+    for (size_t iInnerEpoch = 0; iInnerEpoch < innerEpoch; iInnerEpoch++)
+    {
+      uint8_t oldCurPwm = pwm[iarr];
+      pwm[iarr] = max(min((int)oldCurPwm + v, 255), 0);
+      int newLose = Loss();
+      loseDiff = newLose - lose;
+      //printf("%d ", newLose);
+      if (loseDiff >= 0)
+      {
+        pwm[iarr] = oldCurPwm;
+        direction = -direction;
+      }
+      lose = newLose;
+      if (loseDiff == 0)
+        break;
+      v = direction * abs(loseDiff);
+    }
+    }
+    iarr = (iarr + 1) & ~ARRLEN; // (iarr + 1) % ARRLEN
+  */
 }
 
-void BEMF_A_RISING() {
+void BEMF_A_RISE() {
   ADCSRB = 0;    // Select AIN1 (Pin7) as comparator negative input
-  ACSR |= 3;            // Set interrupt on rising edge
+  //ACSR |= 3;            // Set interrupt on rising edge
 }
-void BEMF_A_FALLING() {
+void BEMF_A_FALL() {
   ADCSRB = 0;    // Select AIN1 (Pin7) as comparator negative input
-  ACSR &= ~1;           // Set interrupt on falling edge
+  //ACSR &= ~1;           // Set interrupt on falling edge
 }
-void BEMF_B_RISING() {
+void BEMF_B_RISE() {
   ADCSRA = 0;   // Disable the ADC module
   ADCSRB = (1 << ACME);
   ADMUX = 2;              // Select analog channel 2 (A2) as comparator negative input
-  ACSR |= 3;
+  //ACSR |= 3;
 }
-void BEMF_B_FALLING() {
+void BEMF_B_FALL() {
   ADCSRA = 0;   // Disable the ADC module
   ADCSRB = (1 << ACME);
   ADMUX = 2;              // Select analog channel 2 (A2) as comparator negative input
-  ACSR &= ~1;
+  //ACSR &= ~1;
 }
-void BEMF_C_RISING() {
+void BEMF_C_RISE() {
   ADCSRA = 0;   // Disable the ADC module
   ADCSRB = (1 << ACME);
   ADMUX = 3;              // Select analog channel 3 (A3) as comparator negative input
-  ACSR |= 3;
+  //ACSR |= 3;
 }
-void BEMF_C_FALLING() {
+void BEMF_C_FALL() {
   ADCSRA = 0;   // Disable the ADC module
   ADCSRB = (1 << ACME);
   ADMUX = 3;              // Select analog channel 3 (A3) as comparator negative input
-  ACSR &= ~1;
+  //ACSR &= ~1;
 }
 
 ////////////////////////////////////////////////
@@ -218,10 +199,10 @@ void CH_BL() {
 }
 
 void SET_PWM_DUTY(byte duty) {
-  if (duty < PWM_MIN_DUTY)
-    duty  = PWM_MIN_DUTY;
-  if (duty > PWM_MAX_DUTY)
-    duty  = PWM_MAX_DUTY;
+//  if (duty < PWM_MIN_DUTY)
+//    duty  = PWM_MIN_DUTY;
+//  if (duty > PWM_MAX_DUTY)
+//    duty  = PWM_MAX_DUTY;
   OCR1A  = duty;                   // Set pin 9  PWM duty cycle
   OCR1B  = duty;                   // Set pin 10 PWM duty cycle
   OCR2A  = duty;                   // Set pin 11 PWM duty cycle
